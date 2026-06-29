7f5b75183e1445b9475befbe7a69917b991470f6
Mika <mikayxcoding@gmail.com>
2026-06-28 23:47:56 -0400

    wip cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use crate::cli::input;
+
+mod cli {
+    use std::io::{self, Write};
+
+    #[must_use]
+    pub fn input(text: &str, default: Option<&str>) -> String {
+        let mut buffer = String::new();
+
+        match default {
+            Some(default) => {
+                let default = default.to_owned();
+
+                print!("{text} ({default}): ");
+                io::stdout().flush().expect("failed to flush stdout");
+
+                io::stdin().read_line(&mut buffer).expect("failed to read");
+                buffer = buffer.trim().to_owned();
+
+                if buffer.len() == 0 { default } else { buffer }
+            }
+            None => {
+                while buffer.len() == 0 {
+                    print!("{text}: ");
+                    io::stdout().flush().expect("failed to flush stdout");
+
+                    io::stdin().read_line(&mut buffer).expect("failed to read");
+                    buffer = buffer.trim().to_owned();
+                }
+
+                buffer
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("cproj - C/C++ project generator by Mikayeek");
+    println!("v{}", env!("CARGO_PKG_VERSION"));
+
+    let folder_name = input("folder name", None);
 }
