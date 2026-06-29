5aae0ce9ea2fe7a603d4e7a9191ac4d2f7972be2
Mika <mikayxcoding@gmail.com>
2026-06-29 00:07:49 -0400

    questions

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,38 @@ mod cli {
             }
         }
     }
+
+    pub fn question(text: &str, default: Option<bool>) -> bool {
+        let mut buffer = String::new();
+
+        match default {
+            Some(default) => loop {
+                buffer.clear();
+                print!("{text} ({})? ", if default { "yes" } else { "no" });
+                io::stdout().flush().expect("failed to flush stdout");
+
+                io::stdin().read_line(&mut buffer).expect("failed to read");
+                match buffer.to_lowercase().trim() {
+                    "yes" => break true,
+                    "no" => break false,
+                    input if input.len() == 0 => break default,
+                    _ => println!("expected 'yes' or 'no'"),
+                }
+            },
+            None => loop {
+                buffer.clear();
+                print!("{text}? ");
+                io::stdout().flush().expect("failed to flush stdout");
+
+                io::stdin().read_line(&mut buffer).expect("failed to read");
+                match buffer.to_lowercase().trim() {
+                    "yes" => break true,
+                    "no" => break false,
+                    _ => println!("expected 'yes' or 'no'"),
+                }
+            },
+        }
+    }
 }
 
 fn main() {
@@ -40,4 +72,5 @@ fn main() {
     let project_name = cli::input("project name", Some(&folder_name));
     let project_version = cli::input("project version", Some("1.0.0"));
     let cmake_version = cli::input("CMake version", Some("3.31"));
+    let use_style = cli::question("use Mika's code formatting style", Some(true));
 }
